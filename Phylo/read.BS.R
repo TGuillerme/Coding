f495888c28c449795776abc330eb7a83e5e6d136
TGuillerme <guillert@tcd.ie>
2014-04-29 17:59:32 +0100

    Bugged version, use first commit
    Cleaning + making the code work for any input tree

--- Phylo/read.BS.R
+++ Phylo/read.BS.R
@@ -6,8 +6,9 @@
 #To do: make the function run for trees in R environment
 ##########################
 #SYNTAX :
-#<pattern> the chain name of the trees to read.
+#<phy> can be either a phylo or multiPhylo object or a chain name of nexus or newick tree out of R environment.
 #<plot> whether to plot the results or not (default = TRUE).
+#<save> whether to save the plot or not (default = FALSE).
 ##########################
 #----
 #guillert(at)tcd.ie - 28/04/2014
@@ -17,29 +18,153 @@
 #########################
 
 
+#IN DEVELOPEMENT USE FIRST COMMIT
 
-read.BS<-function(pattern,plot=TRUE){
-	names<-list.files(pattern=pattern)
-	BS.list<-as.list(names)
-	tree<-read.tree(list.files(pattern=pattern)[1])
-	for (i in 1:length(BS.list)){BS.list[[i]]<-rep(NA,(tree[[2]]))}
-	names(BS.list)<-names
 
-	for (f in 1:length(list.files(pattern=pattern))){
-		trees<-read.tree(list.files(pattern=pattern)[f])
-			for (j in 1: tree[[2]]){
-				BS.list[[f]][j]<-as.numeric(trees[[5]])[j]}
+
+read.BS<-function(phy,plot=TRUE,save=FALSE){
+
+	#IN DEVELOPEMENT USE FIRST COMMIT
+	stop('IN DEVELOPEMENT USE FIRST COMMIT')
+
+	#DATA INPUT
+
+	#phy
+	if(class(phy) ==  "phylo"){
+		single.tree <- TRUE
+		pattern.tree <- FALSE
+	} else {
+		if(class(phy) == "multiPhylo"){
+			single.tree <- FALSE
+			pattern.tree <- FALSE
+		} else {
+			if(class(phy) != "character"){
+				stop("phy bad")
+			} else {
+				pattern.tree <- TRUE
+			}
 		}
-	
+	}
+	if(pattern.tree== TRUE) {
+    	pattern.list<-list.files(pattern=phy)
+	}
+
+	#Read pattern and check if tree is nexus/newick and phylo/multiphylo
+
+
+
+    #plot
+    if(class(plot) != 'logical'){
+        stop('Plot should be TRUE or FALSE')
+    }
+
+    #save
+    if(class(save) != 'logical'){
+        stop('Save should be TRUE or FALSE')
+    }
+
+	#FUNCTION
+
+	FUN.read.BS<-function(phy) {
+		#phy=pattern
+		names<-list.files(pattern=pattern)
+		BS.list<-as.list(names)
+		tree<-read.tree(list.files(pattern=pattern)[1])
+		for (i in 1:length(BS.list)){BS.list[[i]]<-rep(NA,(tree[[2]]))}
+		names(BS.list)<-names
+
+		for (f in 1:length(list.files(pattern=pattern))){
+			trees<-read.tree(list.files(pattern=pattern)[f])
+				for (j in 1: tree[[2]]){
+					BS.list[[f]][j]<-as.numeric(trees[[5]])[j]}
+			}
+	}
+
+	#BOOSTRAP READ
+
+	BS.list<-FUN.read.BS(phy)
+
 	if(plot==TRUE){
-		boxplot(BS.list,las=2,ylab="Boostrap values",main="Trees bootstrap comparison")}
-		else{
-			pdf("Bootstraps.pdf")
-			boxplot(BS.list,las=2,ylab="Boostrap values",main="Trees bootstrap comparison")
-			dev.off()
-		}
-	
+		boxplot(BS.list,las=2,ylab="Boostrap")}
+
+	if(save==TRUE){
+		pdf("Bootstraps.pdf")
+		boxplot(BS.list,las=2,ylab="Boostrap")
+		dev.off()
+	}
+
+	#Output
 	BSlist<-list(Boostraps=unlist(BS.list, use.names=FALSE), Details=BS.list)	
 	return(BSlist)
-	
-	}
+
+#Draft
+
+
+	if(class(data) == 'character') {
+        data.is.chain<-TRUE
+    } else {
+        if(class(data) != 'list') {
+            stop('The data input is not a list')
+        } else {
+            data.is.chain<-FALSE
+        }
+    }
+
+    #If data is a chain name, load the data from the .csv files
+    if(data.is.chain == TRUE) {
+        chain.list<-list.files(pattern=data)
+        data.tmp<-NULL
+        
+        #Load the csv files
+        for (i in 1:length(list.files(pattern=data))) {
+            data.tmp[[i]]<-read.csv(list.files(pattern=data)[i], row.names=1)
+        }
+
+        #Rename the csv files list
+        names(data.tmp)<-unlist(strsplit(chain.list, split=paste('_',data,'.csv',sep='')))
+        data<-data.tmp
+        data.tmp<-NULL
+
+    }
+
+
+}
+#End
+
+
+
+
+
+
+
+
+
+
+
+
+	    if(class(data) == 'character') {
+        data.is.chain<-TRUE
+    } else {
+        if(class(data) != 'list') {
+            stop('The data input is not a list')
+        } else {
+            data.is.chain<-FALSE
+        }
+    }
+
+    #If data is a chain name, load the data from the .csv files
+    if(data.is.chain == TRUE) {
+        chain.list<-list.files(pattern=data)
+        data.tmp<-NULL
+        
+        #Load the csv files
+        for (i in 1:length(list.files(pattern=data))) {
+            data.tmp[[i]]<-read.csv(list.files(pattern=data)[i], row.names=1)
+        }
+
+        #Rename the csv files list
+        names(data.tmp)<-unlist(strsplit(chain.list, split=paste('_',data,'.csv',sep='')))
+        data<-data.tmp
+        data.tmp<-NULL
+
+    }
